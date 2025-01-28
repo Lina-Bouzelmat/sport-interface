6d7bd948b64fdffdbf2d5d6a7f2b16cadeedc304
Lina-Bouzelmat <linabouzelmat@gmail.com>
2025-01-28 23:34:43 +0100

    test Conrtoller Abonnement liaison page twig, mise en place des formulaire des entité ( src/from/.. )

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Abonnement;
+use App\Form\AbonnementType;
+use App\Repository\AbonnementRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class AbonnementController extends AbstractController
+class AbonnementController extends AbstractController
 {
-    #[Route('/abonnement', name: 'app_abonnement')]
-    public function index(): Response
+    #[Route('/abonnement', name: 'abonnement_index')]
+    public function index(Request $request,EntityManagerInterface $entityManager): Response
     {
+        $abonnement = new Abonnement();
+
+        // Crée le formulaire
+        $form = $this->createForm(AbonnementType::class, $abonnement);
+
+        // Gère la requête pour le formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Logique de sauvegarde (si nécessaire)
+            //$entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($abonnement);
+            $entityManager->flush();
+
+            // Redirection après soumission
+            return $this->redirectToRoute('abonnement_index');
+        }
+
         return $this->render('abonnement/index.html.twig', [
-            'controller_name' => 'AbonnementController',
+            'form' => $form->createView(), // Passe le formulaire à la vue
         ]);
     }
+
+    #[Route('/abonnement/new', name: 'abonnement_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $abonnement = new Abonnement();
+        // Créer un formulaire basé sur AbonnementType
+        $form = $this->createForm(AbonnementType::class, $abonnement);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($abonnement);
+            $entityManager->flush();
+            return $this->redirectToRoute('abonnement_index');
+        }
+        return $this->render('abonnement/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
 }
